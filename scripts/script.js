9c80a022009e1437b766c0ba7b81267c73179a15
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-12-15 18:56:18 +0200

    created index.html ; script.js ; README.md

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXNobGVpZ2gtdGFuYWthIiwiYSI6ImNreDdpdTVvNTE4OWYycG81bzJuMjRpMXEifQ.QO8cNP_H-SXWKTvWBU-wfg';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+})
+
+function successLocation(position) {
+    console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap([32.12400, -18.53359])
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15
+    })
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Add map directions to the map
+    map.addControl(
+        new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+        }),
+        'top-left'
+    );
+
+}
